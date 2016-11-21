0257edc464c5495031016c45383f985335d13a87
gbarnis <gbarnis@gmail.com>
2016-11-20 19:05:58 -0500

    Add files via upload

---
+++ Assets/Scripts/CameraControl.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraControl : MonoBehaviour {
+
+    #region Fields
+    public GameObject player;
+    private Vector3 offset;
+    #endregion
+
+    void Start()
+    {
+        //Get the delta of the starting point between the player and camera
+        offset = transform.position - player.transform.position;
+    }
+
+    /// <summary>
+    /// Happens every frame after 
+    /// </summary>
+    void LateUpdate()
+    {
+        //Adjust the camera position to follow the player
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public float speed;
+    private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float moveVertical = Input.GetAxis("Vertical");
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+
+    }
+}
