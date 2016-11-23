e52b15e9ee1e8bbffb51d2d774a5b2a0aaf42caa
gbarnis <gbarnis@gmail.com>
2016-11-23 13:34:34 -0500

    Add files via upload

---
+++ Assets/CameraController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject Player;//In the unity UI - drag the player game object into the "Player" variable inside the camera's script component
+    private Vector3 offset;
+    
+	// Use this for initialization
+	void Start ()
+    {
+        //Get the difference between the camera's position and the player position
+        offset = transform.position - Player.transform.position;
+	}
+	
+    /// <summary>
+    /// Runs once every frame after all items have been processed in update
+    /// </summary>
+	void LateUpdate ()
+    {
+        //Adjusts the position of the camera to follow the player movements
+        transform.position = Player.transform.position + offset;
+	}
+}
---
+++ Assets/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed;//Public global variable - allows modification from the Unity editor 
+    public Text countText;
+    public Text WinText;
+
+    private Rigidbody rb;
+    private int count;
+
+    /// <summary>
+    /// Called on the first frame where the script is active
+    /// </summary>
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        WinText.text = "";
+        setCountText();
+    }
+
+    /// <summary>
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// FixedUpdate should be used instead of Update when dealing with Rigidbody
+    /// </summary>
+    void FixedUpdate()
+    {
+        float moveHorizontaly = Input.GetAxis("Horizontal");
+        float moveVerticaly = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontaly, 0.0f, moveVerticaly);
+        rb.AddForce(movement * Speed);
+    }
+
+    /// <summary>
+    /// Pick up objects
+    /// </summary>
+    /// <param name="other"></param>
+    void OnTriggerEnter(Collider other)
+    {
+        //The pick up objects are defined with rigid body and kinematics
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            setCountText();
+        }
+    }
+
+    void setCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 12)
+            WinText.text = "You Win!";
+    }
+
+}
